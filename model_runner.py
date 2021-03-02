f9839c1e2050c6a4fb425790ffb1024950cfd4ae
orensultan <orens2806@gmail.com>
2021-03-02 17:45:14 +0200

    model eval

--- model_runner.py
+++ model_runner.py
@@ -1,58 +1,29 @@
-import pomegranate
-import numpy as  np
-from pomegranate import (
-    DiscreteDistribution,
-    HiddenMarkovModel,
-    State,
-)
-from possible_observations import possible_observations
+import numpy as np
 from fetch_pdbtm_db import (
     read_training_file
 )
 from pomegranate_model import create_model
+import json
 
-
-
-import itertools
-
-def get_num_tm(s):
-    positions = []
-    for i in range(len(s)):
-        if s[i] == 'I':
-            positions.append(i)
-    return len(list(to_ranges(positions)))
-
-
-def to_ranges(iterable):
-    iterable = sorted(set(iterable))
-    for key, group in itertools.groupby(enumerate(iterable), lambda t: t[1] - t[0]):
-        group = list(group)
-        yield group[0][1], group[-1][1]
-
-
-
-
+num_training_samples = 4000
+test_training_ratio = 0.1
+num_test_samples = int(num_training_samples * test_training_ratio)
+training_file_name = 'training_data.txt'
 
 
 def main():
     model = create_model()
-    observation, labels = read_training_file('training_data.txt')
+    observation, labels = read_training_file(training_file_name)
     observation, labels = np.array(observation), np.array(labels)
-    print(len(observation), len(labels), type(observation))
+    print("number of observations: " + str(len(observation)))
+    print("number of labels: " + str(len(labels)))
+    print("number of training samples: " + str(num_training_samples))
+    print("number of test samples: " + str(num_test_samples))
 
     indices = np.arange(0, len(observation))
-    # np.random.shuffle(indices)
-    train_indices, test_indices = indices[:500], indices[50:]
+    train_indices, test_indices = indices[num_test_samples:num_training_samples], indices[:num_test_samples]
+    _, _ = model.fit(observation[train_indices], labels=labels[train_indices], return_history=True, max_iterations=50,)
 
-    m, history = model.fit(
-        observation[train_indices],
-        labels=labels[train_indices],
-        return_history=True,
-        max_iterations=10,
-        lr_decay=0.001,
-        batches_per_epoch=32,
-    )
-    import json
     with open('model_json_100sam_labeled', 'w') as f:
         json.dump(model.to_json(), f)
 
@@ -60,13 +31,7 @@ def main():
     transition_matrix()
     states = np.array([state.name for state in model.states])
 
-    # print(model.log_probability(observation[0]))
-    # print(history)
-    # print(states)
-    # print(states[model.predict(observation[0])])
-
     print("============= evaluation =============")
-
     print("============= hidden state vs. hidden state =============")
 
     acc_list = []
@@ -88,26 +53,15 @@ def main():
         acc_list.append(round(count_correct / count_total * 100, 2))
 
     acc_np_array = np.array(acc_list)
+    print("samples")
+    for i in range(len(test_indices)):
+        print("prediction sample index: " + str(i))
+        print(pred_actual_list[i][0])
+        print("actual sample index: " + str(i))
+        print(pred_actual_list[i][1])
+        print("accuracy per sample: " + str(acc_list[i]) + "%")
     print("avg acc: " + str(round(np.mean(acc_np_array), 2)))
     print("var acc: " + str(round(np.var(acc_np_array), 2)))
-
-    count_correct, count_total = 0, 0
-    for index in range(len(test_indices)):
-        prediction_list = states[model.predict(observation[test_indices[index]])]
-        binary_prediction_list = ['O' if item == 'B' else 'I' for item in prediction_list[1:-1]]
-        prediction_str = ''.join(binary_prediction_list)
-        ground_truth_list = labels[test_indices[index]]
-        binary_ground_truth_list = ['O' if item == 'B' else 'I' for item in ground_truth_list[1:-1]]
-        ground_truth_str = ''.join(binary_ground_truth_list)
-
-        num_tm_ground_truth = get_num_tm(ground_truth_str)
-        num_tm_prediction = get_num_tm(prediction_str)
-
-        if num_tm_ground_truth == num_tm_prediction:
-            count_correct += 1
-        count_total += 1
-
-    print(round(count_correct / count_total * 100, 2))
 
 
 
--- pomegranate_model.py
+++ pomegranate_model.py
@@ -7,70 +7,70 @@ from pomegranate import (
 from possible_observations import possible_observations
 from fetch_pdbtm_db import get_alpha_helix_subseq_len_dist, get_alpha_helix_subsequences, read_chains
 def create_model():
-  length_dist = get_alpha_helix_subseq_len_dist(get_alpha_helix_subsequences(read_chains('pdbtm')))
+    length_dist = get_alpha_helix_subseq_len_dist(get_alpha_helix_subsequences(read_chains('pdbtm')))
 
-  start_dist = {x: 0 for x in possible_observations}
-  start_dist['$'] = 1
-  start_dist['^'] = 0
-  start_state = State(DiscreteDistribution(start_dist), 'None-start')
-  start_state.distribution.freeze()
-  end_dist = {x: 0 for x in possible_observations}
-  end_dist['^'] = 1
-  end_dist['$'] = 0
-  end_state = State(DiscreteDistribution(end_dist), 'None-end')
-  end_state.distribution.freeze()
+    start_dist = {x: 0 for x in possible_observations}
+    start_dist['$'] = 1
+    start_dist['^'] = 0
+    start_state = State(DiscreteDistribution(start_dist), 'None-start')
+    start_state.distribution.freeze()
+    end_dist = {x: 0 for x in possible_observations}
+    end_dist['^'] = 1
+    end_dist['$'] = 0
+    end_state = State(DiscreteDistribution(end_dist), 'None-end')
+    end_state.distribution.freeze()
 
-  emission_background = DiscreteDistribution({
+    emission_background = DiscreteDistribution({
       **{obs: 1/len(possible_observations) for obs in possible_observations},
       **{'$': 0, '^': 0}
-  })
-  emission_motif = DiscreteDistribution({
+    })
+    emission_motif = DiscreteDistribution({
       **{obs: 1/len(possible_observations) for obs in possible_observations},
       **{'$': 0, '^': 0}
-  })
+    })
 
 
-  background_state = State(emission_background, name='B')
-  num_of_short_motif_states = 15
-  short_motif_states = [
-      State(emission_motif, name=f'SM{i+1}') 
+    background_state = State(emission_background, name='B')
+    num_of_short_motif_states = 15
+    short_motif_states = [
+      State(emission_motif, name=f'SM{i+1}')
       for i in range(num_of_short_motif_states)
-  ]
-  longer_motif_states = [
-      State(emission_motif, name=f'LM{i+1}') 
+    ]
+    longer_motif_states = [
+      State(emission_motif, name=f'LM{i+1}')
       for i in range(num_of_short_motif_states + 1)
-  ]
+    ]
 
-  model = HiddenMarkovModel('trial')
-  model.add_states(short_motif_states)
-  model.add_states(longer_motif_states)
-  model.add_states([background_state])
-  model.add_states([start_state, end_state])
+    model = HiddenMarkovModel('trial')
+    model.add_states(short_motif_states)
+    model.add_states(longer_motif_states)
+    model.add_states([background_state])
+    model.add_states([start_state, end_state])
 
-  p = 0.01
-  prob_len_lower_than_15 = sum(length_dist[:15])
-  model.add_transition(model.start, start_state, 1)
-  model.add_transition(start_state, background_state, 1)
-  model.add_transition(background_state, background_state, 1-p)
-  model.add_transition(background_state, short_motif_states[0], prob_len_lower_than_15*p)
-  model.add_transition(background_state, longer_motif_states[0], (1-prob_len_lower_than_15)*p-0.001)
-  model.add_transition(background_state, end_state, 0.001)
-  model.add_transition(end_state, model.end, 1)
+    p = 0.01
+    prob_len_lower_than_15 = sum(length_dist[:15])
+    model.add_transition(model.start, start_state, 1)
+    model.add_transition(start_state, background_state, 1)
+    model.add_transition(background_state, background_state, 1-p)
+    model.add_transition(background_state, short_motif_states[0], prob_len_lower_than_15*p)
+    model.add_transition(background_state, longer_motif_states[0], (1-prob_len_lower_than_15)*p-0.001)
+    model.add_transition(background_state, end_state, 0.001)
+    model.add_transition(end_state, model.end, 1)
 
-  for i, Mi in enumerate(short_motif_states[:-1]):
+    for i, Mi in enumerate(short_motif_states[:-1]):
       temp_p = length_dist[i]
       model.add_transition(Mi, short_motif_states[i + 1], 1 - temp_p)
       model.add_transition(Mi, background_state, temp_p)
-  model.add_transition(short_motif_states[-1], background_state, 1)
+    model.add_transition(short_motif_states[-1], background_state, 1)
 
-  for i, Mi in enumerate(longer_motif_states[:-1]):
+    for i, Mi in enumerate(longer_motif_states[:-1]):
       model.add_transition(Mi, longer_motif_states[i + 1], 1)
-      
-  q = 0.02
-  model.add_transition(longer_motif_states[-1], longer_motif_states[-1], q)
-  model.add_transition(longer_motif_states[-1], background_state, 1-q)
-  model.bake()
-  return model
+
+    q = 0.02
+    model.add_transition(longer_motif_states[-1], longer_motif_states[-1], q)
+    model.add_transition(longer_motif_states[-1], background_state, 1-q)
+    model.bake()
+    return model
 
 
 
